b8a747283d6c51324a30cc867885a6b288f308f2
Khuong Pham <khuongphampn@gmail.com>
2025-03-05 11:04:22 +0700

    Task 1: Geting all the list of books available

--- final_project/index.js
+++ final_project/index.js
@@ -12,9 +12,22 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+    if(req.session.authenticated){
+        let accessToken = req.session.authenticated['accessToken'];
+        jwt.verify(accessToken, "cpZSR2TaZUlonj5X3EPqg7mxApDKh5wu", (error, user) => {
+            if(!error){
+                req.user = user;
+                next();
+            }else{
+                res.status(403).json({message: "User not authenticated"});
+            }
+        });
+    }else{
+        return res.status(403).json({message: "User not logged in"});
+    }
 });
  
-const PORT =5000;
+const PORT =8096;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
